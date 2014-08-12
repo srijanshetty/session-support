ff5e996a8cce05c0ec1f510f8dc8a3135cf15bf6
Srijan R Shetty <srijan.shetty@gmail.com>
2014-08-12 18:38:16 +0530

    Multithreaded server

--- server.c
+++ server.c
@@ -1,50 +1,87 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 
+// Related to sockets
 #include<arpa/inet.h>
 #include<sys/socket.h>
 
+// Related to threading
+#include<pthread.h>
+
+void *connection_handler(void *);
+
 int main(int argc, char *argv[]) {
+
+    // Create a socket
     int socket_desc = socket(AF_INET, SOCK_STREAM, 0);
     if ( socket_desc == -1 ) {
-        puts("Socket creation failed");
+        perror("[FAIL] Socket creation failed");
         return 1;
     }
-    puts("Socket creation succeded");
+    puts("[OKAY] Socket creation succeded");
 
+    // Bind the socket to the port
     struct sockaddr_in server;
+
     server.sin_addr.s_addr = INADDR_ANY;
     server.sin_family = AF_INET;
     server.sin_port = htons(8000);
-
-    // Bind the socket to the port
     if ( bind(socket_desc, (struct sockaddr *) &server, sizeof(server)) < 0 ) {
-        puts("Bind failed");
+        perror("[FAIL] Bind failed");
     }
-    puts("Bind succeded");
+    puts("[OKAY] Bind succeded");
 
     // Listen for connections
     listen(socket_desc, 3);
 
     // Now we accept connections
-    puts("Waiting for connections");
     struct sockaddr_in client;
-    int c = sizeof(struct sockaddr_in);
-    int new_socket;
     char *message;
+    int new_socket, *new_sock, c;
+
+    puts("[OKAY] Waiting for connections");
+    c = sizeof(struct sockaddr_in);
     while ( (new_socket = accept( socket_desc, (struct sockaddr *) & client, (socklen_t *) &c)) ) {
-        puts("Connection Accepted");
-        message = "Hello World";
+        puts("[OKAY] Connection Accepted");
+        message = "HELO\n";
         write(new_socket, message, strlen(message));
+
+        // Handler
+        pthread_t handler;
+        new_sock = malloc(sizeof(int));
+        *new_sock = new_socket;
+
+        if ( pthread_create( &handler, NULL, connection_handler, (void *)new_sock )  < 0 ) {
+            perror("[FAIL] could not create thread");
+            return 1;
+        }
+
+        // Join the thread
+        // pthread_join( handler, NULL)
+        puts("[OKAY] Handler Assigned");
     }
 
     if ( new_socket < 0 ) {
-        perror("accept failed");
+        perror("[FAIL] accept failed");
         return 1;
     }
-
 
     return 0;
 }
 
+void *connection_handler( void *sock ) {
+    // Copy the socket
+    int socket_desc = *(int *)sock;
+    char *message;
+
+    // Send message to the client
+    message = "FIN\n";
+    write(socket_desc, message, strlen(message));
+
+    // Free the space which was allocated to the socket
+    free(sock);
+
+    return 0;
+}
 
