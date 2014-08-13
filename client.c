3c78ff191f1e9bec89fe3b1369b51ff75bf6957b
Srijan R Shetty <srijan.shetty@gmail.com>
2014-08-13 16:44:43 +0530

    Modified client for the current application

--- client.c
+++ client.c
@@ -3,6 +3,10 @@
 #include<arpa/inet.h>
 #include<string.h>
 
+// Constants
+#define PORT 8000
+#define SERVER "127.0.0.1"
+
 int main(int argc, char *argv[]) {
     // Create a local socket
     int socket_desc = socket(AF_INET, SOCK_STREAM, 0);
@@ -13,31 +17,31 @@ int main(int argc, char *argv[]) {
 
     // Create a connection to the server
     struct sockaddr_in server;
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_addr.s_addr = inet_addr(SERVER);
     server.sin_family = AF_INET;
-    server.sin_port = htons(8000);
+    server.sin_port = htons(PORT);
 
     // Now we send a connect message
     if ( connect(socket_desc, (struct sockaddr *) &server, sizeof(server)) < 0 ) {
-        puts("connection error");
+        perror("[FAIL] Connection Error");
         return 1;
     }
-    puts("Connected");
+    printf("[OKAY] Connected to %s at %d", SERVER, PORT);
 
     // Now we send a message to the server
-    char *msg = "GET / HTTP/1.1\r\n\n\r\n";
+    char *msg = "HELO\n";
     if ( write(socket_desc, msg, strlen(msg)) < 0 ) {
-        puts("Message not sent");
+        perror("[FAIL] Message delivery failed");
         return 1;
     }
-    puts("Message sent");
+    puts("[OKAY] HELO");
 
     char reply[2000];
     if ( read(socket_desc, reply, 2000) < 0 ) {
-        puts("No Reply");
+        puts("[FAIL] No Reply");
         return 1;
     }
-    puts("Reply Recieved");
+    puts("[OKAY] Reply Recieved");
     puts(reply);
 
     close(socket_desc);
